babcb1390601ba7a632d83f59a01047dc746a970
GIGABYTE <nathanaelmoses>
2024-12-24 18:06:25 +0700

    1

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ def query():
     results = None  
     if request.method == 'POST':
         trainee_numb = request.form.get('trainee_number')
+        trainee_pass = request.form.get('trainee_pass')
         connection = get_db_connection()
         if connection is None:
             return "Failed to connect to the database!"
         try:
             with connection.cursor() as cursor:
-                query = "SELECT * FROM trainee WHERE trainee_number = %s"
-                cursor.execute(query, (trainee_numb,))
+                query = "SELECT * FROM trainee WHERE trainee_number = %s trainee_pass = %s"
+                cursor.execute(query, (trainee_numb,trainee_pass))
                 results = cursor.fetchall() 
         finally:
             connection.close()
