2da2f82316f38c713c16b4617e06e95823ad0773
MarcellSutiono <marcell.sutiono@gmail.com>
2024-12-26 10:38:26 +0700

    snow

---
+++ static/JS/snow.js
@@ -0,0 +1,12 @@
+canvas = document.getElementById("snow-canvas");
+ctx = canvas.getContext("2d");
+
+width = canvas.width = window.innerWidth;
+height = canvas.height = window.innerHeight;
+
+const maxSnow = 200;
+var snow = [];
+
+function snowCreate{
+    
+}
