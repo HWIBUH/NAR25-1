63487177c72b678f0e2e67e40360800f64ed40b4
NayutaChan1 <rf05022006@gmail.com>
2024-12-27 22:56:53 +0700

    Subco Benerin, King Kenneth Carry Saya

--- static/JS/subco.js
+++ static/JS/subco.js
@@ -5,11 +5,18 @@ const question = document.querySelector(".question")
 const answer = document.querySelector(".answer")
 
 show.addEventListener("click", (e)=>{
+    console.log("AAAAAAAa")
     question.classList.add("hidden")
     answer.classList.remove("hidden")
 })
 
 prev.addEventListener("click", (e)=>{
+    console.log("BBBBBBBBBB")
+    answer.classList.add("hidden")
+    question.classList.remove("hidden")
+})
+
+next.addEventListener("click", (e) => {
     answer.classList.add("hidden")
     question.classList.remove("hidden")
 })
@@ -21,10 +28,10 @@ async function subco() {
     const soal = document.querySelector(".pertanyaan_subco")
     const jawaban = document.querySelector(".jawaban_subco")
 
-    console.log(soal)
-    console.log(jawaban)
-    console.log(subbco[0].subco_pertanyaan)
-    console.log(subbco[0].subco_jawaban)
+    // console.log(soal)
+    // console.log(jawaban)
+    // console.log(subbco[0].subco_pertanyaan)
+    // console.log(subbco[0].subco_jawaban)
 
     soal.innerText = subbco[0].subco_pertanyaan
     jawaban.innerText = subbco[0].subco_jawaban
@@ -33,6 +40,6 @@ async function subco() {
 }
 
 function generate(){
-    console.log('pppp');
+    // console.log('pppp');
     subco();
 }
